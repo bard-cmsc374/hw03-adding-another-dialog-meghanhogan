a71167108ed0cf54c0fbcf046caa69fb37b252f3
meghanhogan <hoganm24@gmail.com>
2016-10-17 20:34:27 -0400

    Initial commit

---
+++ app/src/androidTest/java/com/bignerdranch/android/criminalintent/AndroidTest2.java
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.criminalintent;
+import android.app.Application;
+import android.test.ApplicationTestCase;
+/**
+ * Created by meghanhogan on 10/17/16.
+ */
+public class AndroidTest2 {
+
+}
---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/PickersFragment.java
@@ -0,0 +1,95 @@
+package com.bignerdranch.android.criminalintent;
+
+
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by meghanhogan on 9/28/16.
+ */
+public class PickersFragment extends DialogFragment{
+
+    public static final String EXTRA_DATE = "com.bignerdranch.android.criminalintent.date";
+
+    private static final String ARG_DATE = "date";
+
+    private DatePicker mDatePicker;
+    private TimePicker mTimePicker;
+
+    public static PickersFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+
+        PickersFragment fragment = new PickersFragment();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
+        mDatePicker = (DatePicker) v.findViewById(R.id.dialog_date_date_picker);
+        mDatePicker.init(year, month, day, null);
+
+        mTimePicker = (TimePicker) v.findViewById(R.id.dialog_time_time_picker);
+        mTimePicker.setMinute(minute);
+        mTimePicker.setHour(hour);
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(v)
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which){
+                                int year = mDatePicker.getYear();
+                                int month = mDatePicker.getMonth();
+                                int day = mDatePicker.getDayOfMonth();
+                                int hour = mTimePicker.getHour();
+                                int minute = mTimePicker.getMinute();
+                                Date date = new GregorianCalendar(year, month, day, hour, minute).getTime();
+                                sendResult(Activity.RESULT_OK, date);
+                            }
+                        })
+                .create();
+    }
+
+    private void sendResult(int resultCode, Date date){
+        if (getTargetFragment() == null){
+            return;
+        }
+
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_DATE, date);
+
+        getTargetFragment().onActivityResult(getTargetRequestCode(), resultCode, intent);
+
+    }
+}
